e2243a14564ada39e99121932adefdfe47475711
BottomTrap <riadhgharbi96@gmail.com>
2019-04-02 16:53:10 +0100

    Started the Panda Behavior thing
    Will check out more alternatives, since I just downloaded the visual tree representation unity asset, and choose between them.

---
+++ Assets/EnemyUnit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SA;
+using RG;
+using Panda;
+
+public class EnemyUnit : MonoBehaviour
+{
+    private PlayerStats playerStats;
+   
+    private void Update()
+    {
+        playerStats = (PlayerStats) GetComponent(typeof(PlayerStats));
+
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+    }
+
+    [Task]
+    void IdleShoot()
+    {
+
+    }
+
+    [Task]
+    void IsHealthLessThan(float health)
+    {
+
+    }
+
+    [Task]
+    void MoveTo()
+    {
+
+    }
+
+
+}
