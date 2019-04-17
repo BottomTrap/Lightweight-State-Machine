306e5b5a852cd8ccf1a25258857cb1d9ad522df7
BottomTrap <riadhgharbi96@gmail.com>
2019-04-17 19:04:35 +0100

    araer
    aerae

--- Assets/Scripts/Player and Units/PlayerMovement.cs
+++ Assets/Scripts/Player and Units/PlayerMovement.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using RG;
-using Panda;
 
 
 //[RequireComponent(typeof(Animator))]
@@ -101,18 +100,7 @@ namespace RG
         }
 
 
-        #region Tasks
-
-        [Task]
-        public bool IsHealthLessThan(float health)
-        {
-            return this.playerstats.Health.Value < health;
-        }
-
-    
         
-
-        #endregion
 
 
     }
