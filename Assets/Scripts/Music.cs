f5926dce58479ef963c9d5667d7fee8000d9b682
BottomTrap <riadhgharbi96@gmail.com>
2019-06-11 09:04:46 +0100

    Fixed Music issue
    ENVIRONMENT
    UII!!
    RAPPORT

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -10,17 +10,27 @@ public class Music : MonoBehaviour
     public AudioClip mainMenuAudio;
     public AudioClip levelAudio;
 
+
     private void Awake()
     {
         audioSource = (AudioSource)GetComponent(typeof(AudioSource));
-        audioSource.time = 0.32f;
     }
     public void Update()
     {
         if (SceneManager.GetActiveScene().name == "Level")
         {
+            //audioSource.time = 0.32f;
             audioSource.clip = levelAudio;
             
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        if (SceneManager.GetActiveScene().name == "MainMenu")
+        {
+            audioSource.clip = mainMenuAudio;
+
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
